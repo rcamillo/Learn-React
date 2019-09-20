b04113a22a449fe2c9cb57d8b0a7e4df4099016d
Renan <renan.camilo@publi9.com.br>
2019-09-20 17:19:20 -0300

    Navegação completa

---
+++ src/pages/main/index.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import api from "../../services/api";
+import { Link } from "react-router-dom";
+
+import "./styles.css";
+
+export default class Main extends Component {
+  state = {
+    products: [],
+    productsInfo: {},
+    page: 1
+  };
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
+
+    const { docs, ...productsInfo } = response.data;
+
+    this.setState({ products: docs, productsInfo, page });
+  };
+
+  prevPage = () => {
+    const { page } = this.state;
+
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+
+    this.loadProducts(pageNumber);
+  };
+
+  nextPage = () => {
+    const { page, productsInfo } = this.state;
+
+    if (page === productsInfo.pages) return;
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
+  };
+
+  render() {
+    const { products, page, productsInfo } = this.state;
+
+    return (
+      <div className="products-list">
+        {products.map(products => (
+          <article key={products._id}>
+            <strong>{products.title}</strong>
+            <p>{products.description}</p>
+
+            <Link to={`/products/${products._id}`}>Acessar</Link>
+          </article>
+        ))}
+        <div className="actions">
+          <button disabled={page === 1} onClick={this.prevPage}>
+            Anterior
+          </button>
+          <button
+            disabled={page === productsInfo.pages}
+            onClick={this.nextPage}
+          >
+            Próximo
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/pages/products/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Products extends Component {
+  state = {
+    products: {}
+  };
+
+  componentDidMount() {
+      
+  }
+
+  render() {
+    return <h1>Products</h1>;
+  }
+}
